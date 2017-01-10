421f1a4f58119e73ea1cadc687fe813869e546e2
Arka Sadhu <ark.sadhu2904@gmail.com>
2017-01-10 16:09:07 +0530

    Started lab 1: pid control

---
+++ Expt1/pid_control/pid_control.ino
@@ -0,0 +1,56 @@
+int ref = 500;
+float sum_error = 0;
+float present_error;
+float old_error;
+const int outPin1= 8;
+const int outPin2 = 9;
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(outPin1, OUTPUT);
+  pinMode(outPin2, OUTPUT);
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+  int pot_val = analogRead(A1);
+  delay(2000);
+  present_error = pot_val -ref;
+  
+  float out_volt = pid(old_error, present_error);
+  Serial.print("Pot val ");
+  Serial.println(pot_val);
+  Serial.print("Error ");
+  Serial.println(present_error);
+  Serial.print("Out volt ");
+  Serial.println(present_error);
+  
+//  Serial.print("Out Val");
+//  Serial.println(out_volt);
+
+  if (out_volt > 0)
+  {
+    analogWrite(outPin1, int(out_volt));
+    analogWrite(outPin2, 0);
+  }
+  else
+  {
+    analogWrite(outPin2, int(out_volt));
+    analogWrite(outPin1, 0);
+  }
+}
+
+float pid(float old_err, float present_err)
+{
+  float kp, ki, kd;
+  kp = .004;
+  ki = 0;
+  kd = 0;
+  float diff_error = present_err - old_err;
+  sum_error += present_err;
+  float out_v = kp * present_err + ki * sum_error + kd * diff_error;
+  return out_v;
+}
+
