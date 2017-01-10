43cb330c3be8e9926e74874874261ba4f4a60397
Arka Sadhu <ark.sadhu2904@gmail.com>
2017-01-10 20:36:39 +0530

    Changed the code to new problem : still need to figure out the actual value relating to 180

--- Expt1/pid_control/pid_control.ino
+++ Expt1/pid_control/pid_control.ino
@@ -4,46 +4,76 @@ float present_error;
 float old_error;
 const int outPin1= 8;
 const int outPin2 = 9;
-
+int pot_val_init;
+int pot_val_fin;
+int some_const_value;
 void setup()
 {
 	Serial.begin(9600);
 	pinMode(outPin1, OUTPUT);
 	pinMode(outPin2, OUTPUT);
 	delay(5000);
+  pot_val_init = analogRead(A1);
+  pot_val_fin = pot_val_init + some_const_value;
 }
 
 void loop()
 {
-	// put your main code here, to run repeatedly:
-	int pot_val = analogRead(A1);
-	delay(4);
-	old_error = present_error;
-	present_error = pot_val -ref;
-	
-	float out_volt = pid(old_error, present_error);
-	Serial.print("Pot val ");
-	Serial.println(pot_val);
-	Serial.print("Error ");
-	Serial.println(present_error);
-	Serial.print("Out volt ");
-	Serial.println(out_volt);
-  
-//  Serial.print("Out Val");
+  int pot_val = analogRead(A1);
+  old_error = present_error;
+  present_error = pot_val - pot_val_fin;
+
+  float out_volt = pid(old_error,present_error);
+//  Serial.print("Pot val ");
+//  Serial.println(pot_val);
+//  Serial.print("Error ");
+//  Serial.println(present_error);
+//  Serial.print("Out volt ");
 //  Serial.println(out_volt);
 
-	if (out_volt < 0)
-	{
-		analogWrite(outPin1, int(-out_volt));
-		analogWrite(outPin2, 0);
-	}
-	else
-	{
-		analogWrite(outPin2, int(out_volt));
-		analogWrite(outPin1, 0);
-	}
-
+  if (out_volt < 0)
+  {
+    analogWrite(outPin1, int(-out_volt));
+    analogWrite(outPin2, 0);
+  }
+  else
+  {
+    analogWrite(outPin1, 0);
+    analogWrite(outPin2, int(out_volt));
+  }
 }
+
+//void loop()
+//{
+//	// put your main code here, to run repeatedly:
+//	int pot_val = analogRead(A1);
+//	delay(4);
+//	old_error = present_error;
+//	present_error = pot_val -ref;
+//	
+//	float out_volt = pid(old_error, present_error);
+//	Serial.print("Pot val ");
+//	Serial.println(pot_val);
+//	Serial.print("Error ");
+//	Serial.println(present_error);
+//	Serial.print("Out volt ");
+//	Serial.println(out_volt);
+//  
+////  Serial.print("Out Val");
+////  Serial.println(out_volt);
+//
+//	if (out_volt < 0)
+//	{
+//		analogWrite(outPin1, int(-out_volt));
+//		analogWrite(outPin2, 0);
+//	}
+//	else
+//	{
+//		analogWrite(outPin2, int(out_volt));
+//		analogWrite(outPin1, 0);
+//	}
+//
+//}
 
 float pid(float old_err, float present_err)
 {
